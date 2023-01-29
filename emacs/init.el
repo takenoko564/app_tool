d1087a017c766e24d6c898d0279cb508f73f614a
ryo <ryo@localhost.localdomain>
2023-01-29 23:54:36 +0900

    update init.el

--- emacs/init.el
+++ emacs/init.el
@@ -36,6 +36,19 @@
          (height              . 60))
        initial-frame-alist))
 
+;; Reload function
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  ;;(message "force-reverting value is %s" force-reverting)
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
+(global-set-key (kbd "<f5>") 'revert-buffer-no-confirm)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setting for verilog mode
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
